30779bde35fcb34032422f5dcab13055a9849535
Scott Wichser <blast007@users.sourceforge.net>
2011-12-24 11:56:37 -0600

    Add a COPYING file with the LGPL license. Add a proper header to the plugin file. Remove the 'DESCRIPTION' fom playerdb.def.

--- playerdb.cpp
+++ playerdb.cpp
@@ -1,5 +1,14 @@
-// playerdb.cpp : Defines the entry point for the DLL application.
-//
+/* playerdb
+ * Copyright (c) 2011 Scott Wichser
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
